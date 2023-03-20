4cfbf781d727aebf58a9e6da5ec7a4ded48444fc
hyenni2-2 <hi.choi1031@gmail.com>
2023-03-20 21:28:51 +0900

    jdbc

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,27 +1,37 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
 
     /*
-    * MemberRepository는 인터페이스고
-    * MemoryMemberRepository가 구현체이므로 리턴값에 넣는다.
-    * 인터페이스는 new가 안되기 떄문에.
-    * */
+     * MemberRepository는 인터페이스고
+     * MemoryMemberRepository가 구현체이므로 리턴값에 넣는다.
+     * 인터페이스는 new가 안되기 떄문에.
+     * */
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 
 }
